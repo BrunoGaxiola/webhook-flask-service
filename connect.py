c9b8d2d54cb57fa0e69af8788dffb7ab45d33a0a
BrunoGaxiola <A01253874@tec.mx>
2026-02-04 10:11:43 -0700

    feat: db select query to get endpoint, pending to test.

--- connect.py
+++ connect.py
@@ -4,6 +4,5 @@ def connectToDB(driver, url, database, user, password):
     try:
         connection = pyodbc.connect(f"DRIVER={driver};SERVER={url};DATABASE={database};UID={user};PWD={password}")
         print("Se ha conectado con la base de datos con éxito.")
-        connection.close()
     except Exception as e:
         print("Hubo error conectándose con la base de datos.", e)
